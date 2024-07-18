4a347c41afa5f2b8a2fedeccfb052acdaedbfec8
MARCE12CAL <calvachemarli@gmail.com>
2024-07-17 20:22:00 -0500

    as

--- trabajo/views/html/menu.php
+++ trabajo/views/html/menu.php
@@ -5,12 +5,19 @@
                 <i class="fas fa-code me-2"></i>PROYECTO
             </h3>
         </a>
-    
+        <a href="<?php __DIR__ . '/../dashboard.php'; ?>" class="nav-item nav-link"><i class="fa fa-tachometer-alt me-2"></i>MENÚ</a>
+            <a href='./amigos.php' class='nav-item nav-link'><i class='fa fa-th me-2'></i>Amigos</a>
+            <a href="./grupos.php" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Grupos</a>
+            <a href="./miembros_grupos.php" class='nav-item nav-link'><i class='fa fa-th me-2'></i>Miembros Grupos</a>
+            <a href='./publicaciones.php' class='nav-item nav-link'><i class='fa fa-th me-2'></i>Publicaciones</a>
+
         <ul class="nav flex-column w-100 mt-4">
             <li class="nav-item w-100">
+            
                 <a href="./amigos.php" class="nav-link text-light py-3 px-4 w-100 d-flex align-items-center">
                     <i class="fas fa-user-friends me-3 fs-5"></i>
                     <span>Amigos</span>
+                 
                 </a>
             </li>
         </ul>
